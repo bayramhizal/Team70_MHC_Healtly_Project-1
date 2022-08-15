6ebd88b197ab7f32bd99c4edf48a529cdcbeffd4
Adem Polat <polatadem.2541@gmail.com>
2022-08-15 20:17:28 +0200

    ademcommit

--- src/test/java/stepDefinitions/uiStepDefinitions/US_011.java
+++ src/test/java/stepDefinitions/uiStepDefinitions/US_011.java
@@ -3,25 +3,24 @@ package stepDefinitions.uiStepDefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.apache.http.util.Asserts;
 import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import pages.DoktorPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
-import java.sql.DriverManager;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static utilities.ReusableMethods.waitFor;
 
 public class US_011 {
 
     DoktorPage doktorPage = new DoktorPage();
-    Select select;
+    JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
 
     @Given("kullanici {string} e gider.")
     public void kullanici_e_gider(String medunnaUrl) {
@@ -30,13 +29,14 @@ public class US_011 {
 
     @Then("kullanici Sign In butonunu bulur ve tiklar")
     public void kullanici_sign_in_butonunu_bulur_ve_tiklar() {
+        doktorPage.kullaniciResmibutonu.click();
         doktorPage.signInButton.click();
     }
 
     @Then("kullanici doktor plarak username ve password text boxlarina dogru bilgileri girer")
     public void kullanici_doktor_plarak_username_ve_password_text_boxlarina_dogru_bilgileri_girer() {
         doktorPage.usernameBox.sendKeys(ConfigReader.getProperty("doktorUsername"));
-        doktorPage.usernameBox.sendKeys(ConfigReader.getProperty("doktorPassword"));
+        doktorPage.passwordBox.sendKeys(ConfigReader.getProperty("doktorPassword"));
     }
 
     @Then("Sign in butonunu tiklar")
@@ -62,6 +62,7 @@ public class US_011 {
     @Then("Doktor patent id,start and end date,Status,physician and patient butonlarini gorur")
     public void doktor_patent_id_start_and_end_date_status_physician_and_patient_butonlarini_gorur() {
         Driver.getDriver().navigate().back();
+        waitFor(2);
         Assert.assertTrue(doktorPage.idButton.isDisplayed());
         Assert.assertTrue(doktorPage.endDateTimeButton.isDisplayed());
         Assert.assertTrue(doktorPage.startDateTimeButton.isDisplayed());
@@ -73,6 +74,7 @@ public class US_011 {
 
     @Then("Doktor hasta bilgilerinin dolu oldugunu test eder")
     public void doktor_hasta_bilgilerinin_dolu_oldugunu_test_eder() {
+        waitFor(2);
         List<WebElement> hastaBilgileriGuncel = doktorPage.hastaBilgileriList;
         for (WebElement each : hastaBilgileriGuncel) {
             System.out.println(each.getText());
@@ -82,19 +84,17 @@ public class US_011 {
 
     @And("Doktor patent id,start and end date,Status textboxlarinin dolu oldugunu gorur.")
     public void doktorPatentIdStartAndEndDateStatusTextboxlarininDoluOldugunuGorur() {
+        waitFor(2);
         doktorPage.patientAhmEditButton.click();
-        List<WebElement> textBoxKontrolList = doktorPage.IdStartEndTimesTextBoxList;
-        for (WebElement each : textBoxKontrolList) {
-            Assert.assertFalse(each.getText().isEmpty());
+        Assert.assertFalse(doktorPage.IdStartEndTimesTextBoxList.isEmpty());
 
-
-        }
 
     }
 
 
     @Then("Doktor Anmnesis,Treatment ve Diagnosis sekmelerini gorur")
     public void doktor_anmnesis_treatment_ve_diagnosis_sekmelerini_gorur() {
+        waitFor(2);
         Assert.assertTrue(doktorPage.anamnesisButton.isDisplayed());
         Assert.assertTrue(doktorPage.treatmentButton.isDisplayed());
         Assert.assertTrue(doktorPage.diagnosisButton.isDisplayed());
@@ -102,6 +102,7 @@ public class US_011 {
 
     @Then("Doktor Anmnesis,Treatment ve Diagnosis alanlari doldurur ve bos olmadigi test eder.")
     public void doktor_anmnesis_treatment_ve_diagnosis_alanlari_doldurur_ve_bos_olmadigi_test_eder() {
+        waitFor(2);
         doktorPage.anamnesisTextButton.sendKeys("..");
         doktorPage.treatmentTextButton.sendKeys("..");
         doktorPage.diagnosisTextButton.sendKeys("..");
@@ -112,14 +113,20 @@ public class US_011 {
 
     @Then("bu alanlardan Diagnosis alani kasitli olarak bos birakir")
     public void bu_alanlardan_diagnosis_alani_kasitli_olarak_bos_birakir() {
+        waitFor(2);
         doktorPage.diagnosisTextButton.clear();
     }
 
     @Then("Saved butonuna basar ve guncellemenin basarili olmadigini test eder")
     public void saved_butonuna_basar_ve_guncellemenin_basarili_olmadigini_test_eder() {
-        doktorPage.saveButton.click();
+        waitFor(2);
+        jse.executeScript("arguments[0].scrollIntoView(true);", doktorPage.saveButton);
+        jse.executeScript("arguments[0].click();", doktorPage.saveButton);
+        // doktorPage.saveButton.click();
+        waitFor(1);
         String expectedResult = "This field is required.";
         String actualResult = doktorPage.fielRequiredButton.getText();
+        System.out.println(doktorPage.fielRequiredButton.getText());
         Assert.assertEquals(expectedResult, actualResult);
     }
 
@@ -131,46 +138,73 @@ public class US_011 {
 
     @Then("Doktor Status sekmesinden PENDING,COMPLETED,CANCELLED olarak secebilir")
     public void doktor_status_sekmesinden_pending_completed_cancelled_olarak_secebilir() {
-        select = new Select(doktorPage.statusSelectButton);
+        waitFor(2);
+        jse.executeScript("arguments[0].scrollIntoView(true);", doktorPage.statusSelectButton);
+        jse.executeScript("arguments[0].click();", doktorPage.statusSelectButton);
+        //doktorPage.statusSelectButton.click();
+        waitFor(1);
+        Select select = new Select(doktorPage.statusSelectButton);
+       // doktorPage.statusSelectButton.click();
         List<WebElement> statusList = select.getAllSelectedOptions();
-        statusList.forEach(t -> System.out.println(t.getText()));
-        select.deselectByVisibleText("PENDING");
+        //statusList.stream().forEach(t -> System.out.println(t.getText()));
+
+        for (WebElement each : statusList
+        ) {
+            System.out.println(each.getText());
+        }
+        select.selectByVisibleText("PENDING");
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.ARROW_UP).sendKeys(Keys.ARROW_UP).perform();
+        waitFor(4);
     }
 
     @Then("Doktor Status sekmesini CANCELLED olarak secer")
     public void doktor_status_sekmesini_cancelled_olarak_secer() {
-        select = new Select(doktorPage.statusSelectButton);
-        select.deselectByVisibleText("CANCELLED");
+        waitFor(2);
+        // jse.executeScript("arguments[0].scrollIntoView(true);",doktorPage.statusSelectButton);
+        waitFor(1);
+
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.UP).perform();
+        //jse.executeScript("arguments[0].click();",doktorPage.statusSelectButton);
+        Select select = new Select(doktorPage.statusSelectButton);
+        //  doktorPage.statusSelectButton.click();
+        select.selectByVisibleText("CANCELLED");
+        waitFor(1);
         String expectedResult = "CANCELLED";
-        String actualResult=doktorPage.statusSelectButton.getText();
+        waitFor(2);
+        String actualResult = select.getFirstSelectedOption().getText();
         Assert.assertEquals(expectedResult, actualResult);
+        System.out.println("=====================================");
+        System.out.println(doktorPage.statusSelectButton.getText());
     }
 
 
     @Then("Prescription ve Description alanlari bos birakilir.")
     public void prescription_ve_description_alanlari_bos_birakilir() {
+        waitFor(2);
         doktorPage.prescriptionTextButton.clear();
         doktorPage.descriptionTextButton.clear();
     }
 
     @Then("Doktor Save butonunu bulup click yapar")
     public void doktor_save_butonunu_bulup_click_yapar() {
-      doktorPage.saveButton.click();
+        jse.executeScript("arguments[0].scrollIntoView(true);", doktorPage.saveButton);
+        jse.executeScript("arguments[0].click();", doktorPage.saveButton);
     }
 
     @Then("Doktor Appointments is updated yazisini dogrular")
     public void doktor_appointments_is_updated_yazisini_dogrular() {
-      // Driver.getDriver().navigate().back();
-       doktorPage.upLoadButton.click();
-       waitFor(1);
-       String arananCumle="Appointments is updated";
-       String actualResult = doktorPage.upLoadButton.getText();
-       Assert.assertTrue(actualResult.contains(arananCumle));
+        waitFor(2);
+        String arananCumle = "Appointment is updated";
+        String actualResult = doktorPage.upLoadButton.getText();
+        System.out.println(actualResult);
+        Assert.assertTrue(actualResult.contains(arananCumle));
     }
 
     @Then("kullanici sayfayi kapatir")
     public void kullanici_sayfayi_kapatir() {
-       Driver.closeDriver();
+        // Driver.closeDriver();
     }
 
 
