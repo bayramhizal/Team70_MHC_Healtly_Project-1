8782aca6dac4ae5159549c6b61452b57f62b435f
seyfullah merdan <merdanseyfullah33@gmail.com>
2022-08-15 20:23:43 +0200

    US01DatabaseClassıOlustu

--- src/test/java/stepDefinitions/jdbcsStepDefinitions/US01_US02.java
+++ src/test/java/stepDefinitions/jdbcsStepDefinitions/US01_US02.java
@@ -1,8 +1,22 @@
 package stepDefinitions.jdbcsStepDefinitions;
 
+import io.cucumber.java.en.Given;
+import utilities.DBUtils;
+
+import static utilities.DBUtils.createConnection;
+
 public class US01_US02 {
 
+    DBUtils database=new DBUtils();
 
+
+
+    @Given("kullanici database baglanir")
+    public void kullanici_database_baglanir() {
+
+        createConnection();
+
+    }
 
 
 
@@ -25,3 +39,19 @@ public class US01_US02 {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/resources/features/us_02/US02_01_DB.feature
@@ -0,0 +1,14 @@
+Feature: DB ile Sorgulama
+
+
+  @dbsm
+  Scenario: DB ile SSN kimlikleri dogrulanir
+
+    Given kullanici database baglanir
+
+
+
+
+  Scenario: Email DB ile dogrulanir
+
+    Given kullanici database baglanir
