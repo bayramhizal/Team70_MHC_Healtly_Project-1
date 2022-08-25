afc8fb7809789caf9afe1580c4081ff87f27e4bf
maozfidan <101925509+maozfidan@users.noreply.github.com>
2022-08-25 23:44:29 +0200

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,25 +6,25 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin={"html:target/cucumber-reports.html",
-                "json:target/json-reports/cucumber.json",
-                "junit:target/xml-report/cucumber.xml"
+        plugin={"html:target/cucumber-reports-testrunner.html",
+                "json:target/json-reports/cucumber-testrunner.json",
+                "junit:target/xml-report/cucumber-testrunner.xml"
         },
         features = "src/test/resources/features",
-        glue= "stepDefinitions",
-        tags= "@US24",
+        glue= {"stepDefinitions","hooks"},
+        tags= "@pasif",
         dryRun = false
 )
 
 public class TestRunner {
     /*
-    Bir framework'de bir tek Runner class'i yeterli olabilir
-    Runner class'inda class body'sinde hic bir sey olmaz
-    Runner class'imizi onemli yapan 2 adet annotaion vardir
-    @RunWith(Cucumber.class) notasyonu Runner class'ina calisma ozelligi katar
+    Bir framework'de bir tek TestRunner3 class'i yeterli olabilir
+    TestRunner3 class'inda class body'sinde hic bir sey olmaz
+    TestRunner3 class'imizi onemli yapan 2 adet annotaion vardir
+    @RunWith(Cucumber.class) notasyonu TestRunner3 class'ina calisma ozelligi katar
     Bu notasyon oldugu icin Cucumber framework'umuzde JUnit kullanmayi tercih ediyoruz
 
-    features : Runner dosyasinin feature dosyalarini nereden bulacagini tarif eder
+    features : TestRunner3 dosyasinin feature dosyalarini nereden bulacagini tarif eder
     glue : step definitions dosyalarini nerede bulacagimizi gosterir
     tags : o an hangi tag'i calistirmak istiyorsak onu belli eder
 
